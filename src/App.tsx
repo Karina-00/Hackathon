e486be6df209d703f9177b778494e4192be8aeda
Kacper <kacper.laudanski@wp.pl>
2022-11-04 20:55:06 +0100

    thrt

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
   return (
       <Router>
           <div className="App">
-              <Navbar/>
+              <Navbar
+                cash={3}
+              />
               <Routes>
                   {/*if not logged in display only the authentication page - try to do it better*/}
                   <Route path="/" element={loggedIn ? <ProfilePage/> : <AuthenticationPage/>}/>
