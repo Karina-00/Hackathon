56e72527b419dd117e1c08af85e68877ddc96990
Kacper <kacper.laudanski@wp.pl>
2022-11-05 02:00:40 +0100

    mama

--- src/components/profile/ProfilePage.tsx
+++ src/components/profile/ProfilePage.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./profile_page.css";
 import ProfilePicture from '../../assets/avatar_placeholder.png';
-import {useAppSelector} from "../../hooks";
+import LoginImage from '../../assets/loginImg.png'; 
+import {useAppSelector} from "../../hooks"; 
+
 
 const ProfilePage = () => {
     const username = useAppSelector((state) => state.userSlice.username);
     return (
-        <div className="ProfilePage">
-            <div className="ImageCard">
-                <img className="ProfilePicture" src={ProfilePicture} alt={"Avatar"}/>
-            </div>
-            <div className="PageContent">
-                <h1> {username} ($100) </h1>
-                <h2>Awards</h2>
-                <div className="ItemsList"> Awards content </div>
-                <h2>Storage</h2>
-                <div className="ItemsList">Storage content</div>
-            </div>
-        </div>
+       <div className="ProfilePage">
+         <div className="UserSide">
+           <h1 className="ProfileHeader">Profile</h1>
+           <img src={LoginImage} alt="user-avatar" className="avatar"></img>
+           <h2 className="UserName">{username}</h2>
+         </div>
+         <div className="AwardsSide">
+            
+         </div>
+       </div>
     )
 
 }
